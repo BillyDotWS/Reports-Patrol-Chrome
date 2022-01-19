b43a946b83a7eafa5f5a4bf9da6dc074e391c93c
Billy Robinson <41408185+CookieBilly@users.noreply.github.com>
2022-01-19 10:35:45 +0000

    Update content.js

--- js/content.js
+++ js/content.js
@@ -4,73 +4,74 @@ try {
     let ruleBreaker = texts.find(txt => txt.startsWith('Name of Rule Breaker(s):'))?.split(':')?.[1].trim()
     let brokenRule = texts.find(txt => txt.startsWith('Broken Rule'))?.split(':')?.[1].trim()
     
-    console.log(ruleBreaker)
-    console.log(brokenRule)
-    
-    sidebar.insertAdjacentHTML(
-        'beforeend',
-        `<div class="section widget-single">
-            <div class="secondaryContent widget sidebar-widget WidgetFramework_WidgetRenderer_Threads">
-                <h3 style="background-color: #1e252f;width: 102%;height: 150%;margin-left: -4%; padding: 6pt !important;
-    margin-right: 100p;/* margin-bottom: 0pt !important; */border-bottom: 4px solid rgb(250, 159, 1);">Report Actions</h3>
-                <div class="options">
-                    <button style="color: #fff;background-color: #28a745;border-color: #28a745;" class="button success ext-mark-accepted">✔ Mark Accepted</button>
-                    <button style="color: #fff;background-color: #dc3545;border-color: #dc3545;" class="button danger ext-mark-denied">❌ Mark Rejected</button>
-                    <button class="button primary ext-mark-bedrock">Mark Bedrock</button>
-                    <button class="button primary ext-mark-no-evidence">No Evidence</button>
-                    <button class="button primary ext-show-options">Options</button>
-                </div>
-                <strong class="ext-form-title">Add Punishment</strong>
-                <hr>
-                <div class="ext-form-container">
-                    <div class="ext-form-row">
-                        <label>Offender</label>
-                        <input type="text" value="${ruleBreaker}" id="ext-offender">
-                    </div>
-                    <div class="ext-form-row">
-                        <label>Offence</label>
-                        <input type="text" value="${brokenRule}" id="ext-offence">
-                    </div>
-                    <div class="ext-form-row">
-                        <label>Category</label>
-                        <select id="ext-category">
-                            <option value="1">Chat Offense</option>
-                            <option value="2">General Offense</option>
-                            <option value="3">Client Mod</option>
-                        </select>
-                    </div>
-                    <div class="ext-form-row">
-                        <label>Severity</label>
-                        <select id="ext-severity">
-                            <option value="1">Severity 1</option>
-                            <option value="2">Severity 2</option>
-                            <option value="3">Severity 3</option>
-                            <option value="4">Permanent</option>
-                        </select>
-                    </div>
-                    <div class="ext-form-row">
-                        <label>Status</label>
-                        <select id="ext-status">
-                            <option value="1">Accepted</option>
-                            <option value="2">Rejected</option>
-                            <option value="3">Incorrect</option>
-                        </select>
-                    </div>
-                    <div class="ext-form-row">
-                        <div class="ext-alert ext-hidden"></div>
-                        <button class="add-punishment-btn">Add</button>
-                    </div>
-                </div>
-            </div>
-        </div>`
-    )
+    if(ruleBreaker != undefined && brokenRule != undefined) {
 
-    document.querySelector('.ext-mark-accepted').onclick = markAccepted
-    document.querySelector('.ext-mark-denied').onclick = markDenied
-    document.querySelector('.add-punishment-btn').onclick = reportPunishment
-    document.querySelector('.ext-mark-bedrock').onclick = markBedrock
-    document.querySelector('.ext-mark-no-evidence').onclick = markNoEvidence
-    document.querySelector('.ext-show-options').onclick = showOptions
+        sidebar.insertAdjacentHTML(
+            'beforeend',
+            `<div class="section widget-single">
+                <div class="secondaryContent widget sidebar-widget WidgetFramework_WidgetRenderer_Threads">
+                    <h3 style="background-color: #1e252f;width: 102%;height: 150%;margin-left: -4%; padding: 6pt !important;
+        margin-right: 100p;/* margin-bottom: 0pt !important; */border-bottom: 4px solid rgb(250, 159, 1);">Report Actions</h3>
+                    <div class="options">
+                        <button style="color: #fff;background-color: #28a745;border-color: #28a745;" class="button success ext-mark-accepted">✔ Mark Accepted</button>
+                        <button style="color: #fff;background-color: #dc3545;border-color: #dc3545;" class="button danger ext-mark-denied">❌ Mark Rejected</button>
+                        <button class="button primary ext-mark-bedrock">Mark Bedrock</button>
+                        <button class="button primary ext-mark-no-evidence">No Evidence</button>
+                        <button class="button primary ext-show-options">Options</button>
+                    </div>
+                    <strong class="ext-form-title">Add Punishment</strong>
+                    <hr>
+                    <div class="ext-form-container">
+                        <div class="ext-form-row">
+                            <label>Offender</label>
+                            <input type="text" value="${ruleBreaker}" id="ext-offender">
+                        </div>
+                        <div class="ext-form-row">
+                            <label>Offence</label>
+                            <input type="text" value="${brokenRule}" id="ext-offence">
+                        </div>
+                        <div class="ext-form-row">
+                            <label>Category</label>
+                            <select id="ext-category">
+                                <option value="1">Chat Offense</option>
+                                <option value="2">General Offense</option>
+                                <option value="3">Client Mod</option>
+                            </select>
+                        </div>
+                        <div class="ext-form-row">
+                            <label>Severity</label>
+                            <select id="ext-severity">
+                                <option value="1">Severity 1</option>
+                                <option value="2">Severity 2</option>
+                                <option value="3">Severity 3</option>
+                                <option value="4">Permanent</option>
+                            </select>
+                        </div>
+                        <div class="ext-form-row">
+                            <label>Status</label>
+                            <select id="ext-status">
+                                <option value="1">Accepted</option>
+                                <option value="2">Rejected</option>
+                                <option value="3">Incorrect</option>
+                            </select>
+                        </div>
+                        <div class="ext-form-row">
+                            <div class="ext-alert ext-hidden"></div>
+                            <button class="add-punishment-btn">Add</button>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+        )
+
+        document.querySelector('.ext-mark-accepted').onclick = markAccepted
+        document.querySelector('.ext-mark-denied').onclick = markDenied
+        document.querySelector('.add-punishment-btn').onclick = reportPunishment
+        document.querySelector('.ext-mark-bedrock').onclick = markBedrock
+        document.querySelector('.ext-mark-no-evidence').onclick = markNoEvidence
+        document.querySelector('.ext-show-options').onclick = showOptions
+        
+    }
 } 
 catch (error) {
     console.log(error)
